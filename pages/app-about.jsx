f217a0370f479c5ec52f813fc7df50e348f83465
Guy <guyelizarov@gmail.com>
2022-04-27 14:52:16 +0300

    guy's wiring bookapp 3

--- pages/app-about.jsx
+++ pages/app-about.jsx
@@ -4,6 +4,8 @@ export function AppAbout(){
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati qui, blanditiis vel maiores maxime dolorem? Accusantium, sapiente quos. Dicta magni tempore neque est eum dolore rem dolorem earum ullam a
 b.lo Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora possimus optio commodi accusantium assumenda minus eos tenetur delectus atque labore numquam, in iusto. Totam, autem? Laborum ipsam adipisci amet beatae!
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aperiam, natus, sit, distinctio sapiente aliquid sequi facere in fuga asperiores aliquam quam quae! Fugiat, perferendis tempora magnam eum nam vero.
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, qui vitae aspernatur error omnis veniam et. Iusto soluta nam ratione laboriosam ex similique quisquam accusantium? Repudiandae voluptatum reprehenderit laboriosam? Cumque!
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, qui vitae
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis dolor, fuga labore vero incidunt voluptas velit repudiandae soluta itaque ullam, quisquam sit molestias mollitia inventore in quae minima vel. Unde?
+ aspernatur error omnis veniam et. Iusto soluta nam ratione laboriosam ex similique quisquam accusantium? Repudiandae voluptatum reprehenderit laboriosam? Cumque!
     </section>
 }

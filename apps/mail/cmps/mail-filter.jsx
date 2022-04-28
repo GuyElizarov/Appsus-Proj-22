a4d8536ba2cf99aa397f951f507d9fce270e1d71
Guy <guyelizarov@gmail.com>
2022-04-28 14:10:40 +0300

    guy's mail filter change

--- apps/mail/cmps/mail-filter.jsx
+++ apps/mail/cmps/mail-filter.jsx
@@ -50,7 +50,7 @@ export class MailFilter extends React.Component {
                         onChange={this.handleChange} ref={this.inputRef} placeholder="Search mail" />
 
                     <select onChange={this.handleChange} value={isRead} name="isRead">
-                        <option value="all" >All</option>
+                        {/* <option value="all" >All</option> */}
                         <option value={true} >Read</option>
                         <option value={false} >Unread</option>
                     </select>

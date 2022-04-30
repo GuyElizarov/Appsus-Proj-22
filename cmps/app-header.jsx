be314ca0f8029e90d4da7b25fd98abfd59fab516
Guy <guyelizarov@gmail.com>
2022-04-30 22:41:14 +0300

    Guy's css commit

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -3,6 +3,8 @@ import { keepImg } from '../assets/imgs/google-keep.png'
 import { booksImg } from '../assets/imgs/google-books.png'
 import { mailImg } from '../assets/imgs/google-mail.png'
 import { homeImg } from '../assets/imgs/google-home.png'
+// import { homeImg } from '../assets/imgs/menu.png'
+import { menuImg } from '../assets/imgs/menu.png'
 
 
 class _AppHeader extends React.Component {
@@ -22,7 +24,7 @@ class _AppHeader extends React.Component {
                 <img className='app-logo' src='../assets/imgs/appsus-logo.png' />
             </div></NavLink>
             <nav>
-                <img className='menu-toggler' onClick={this.onToggleNavMenu} src='../assets/imgs/menu.png' />
+                <img className='menu-toggler' onClick={this.onToggleNavMenu} src={menuImg} />
 
                 <div className={this.state.isNavOpen ? 'nav-container open' : 'nav-container'}>
                     <NavLink onClick={this.onToggleNavMenu} to="/note"><img src={keepImg} /></NavLink>

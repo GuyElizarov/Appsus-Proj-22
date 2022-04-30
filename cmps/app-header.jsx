459caacdd4d45f65063cb37b33829888b3bd46ce
Guy <guyelizarov@gmail.com>
2022-04-30 23:18:17 +0300

    v

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -12,20 +12,20 @@ class _AppHeader extends React.Component {
 
     render() {
         return <header className="app-header layout">
-            {/* <NavLink to="/"><div className='app-logo-wrapper'>
-                <img className='app-logo' src='../assets/imgs/appsus-logo.png' />
+            <NavLink to="/"><div className='app-logo-wrapper'>
+                <img className='app-logo' src='../assets/imgs/appsus-logo.PNG' />
             </div></NavLink>
             <nav>
-                <img className='menu-toggler' onClick={this.onToggleNavMenu} src='../assets/imgs/menu-button.png' />
+                <img className='menu-toggler' onClick={this.onToggleNavMenu} src='../assets/imgs/menu-button.PNG' />
 
                 <div className={this.state.isNavOpen ? 'nav-container open' : 'nav-container'}>
-                    <NavLink onClick={this.onToggleNavMenu} to="/note"><img src='../assets/imgs/google-keep.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/book"><img src='../assets/imgs/google-books.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/mail"><img src='../assets/imgs/google-mail.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/" exact><img src='../assets/imgs/google-home.png' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/note"><img src='../assets/imgs/google-keep.PNG' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/book"><img src='../assets/imgs/google-books.PNG' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/mail"><img src='../assets/imgs/google-mail.PNG' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/" exact><img src='../assets/imgs/google-home.PNG' /></NavLink>
                 </div>
                 {this.state.isNavOpen && <div className='nav-shadow' onClick={this.onToggleNavMenu}></div>}
-            </nav> */}
+            </nav>
         </header>
     }
 }

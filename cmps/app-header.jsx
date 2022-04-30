e3c7cb1dca942cdbc542d6c553489a74c05685b1
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-04-30 22:11:59 +0300

    eytans commit

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -22,17 +22,16 @@ class _AppHeader extends React.Component {
                 <img className='menu-toggler' onClick={this.onToggleNavMenu} src='../assets/imgs/menu.png' />
 
                 <div className={this.state.isNavOpen ? 'nav-container open' : 'nav-container'}>
-
-                    <NavLink onClick={this.onToggleNavMenu} to="/note"><img src='../assets/imgs/google-keep.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/about"> <img src='../assets/imgs/google-about.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/book"><img src='../assets/imgs/google-books.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/mail"><img src='../assets/imgs/google-mail.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/" exact><img src='../assets/imgs/google-home.png' /></NavLink>
-
+                    <NavLink onClick={this.onToggleNavMenu} to="/note"><img src="../assets/imgs/google-keep.png" alt="" /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/book"><img src="../assets/imgs/google-books.png" alt="" /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/mail"><img src="../assets/imgs/google-mail.png" alt="" /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/" exact><img src="../assets/imgs/google-home.png" alt="" /></NavLink>
                 </div>
                 {this.state.isNavOpen && <div className='nav-shadow' onClick={this.onToggleNavMenu}></div>}
             </nav>
         </header>
     }
 }
+
+import { } from '../assets/imgs/menu.png'
 export const AppHeader = withRouter(_AppHeader)
--- pages/app-home.jsx
+++ pages/app-home.jsx
@@ -1,9 +1,6 @@
-export function AppHome(){
+export function AppHome() {
 
     return <section className="app-home">
-        Home
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum delectus eaque quasi aliquam voluptates, at, cupiditate sint, enim dolorem voluptatum exercitationem neque! Dignissimos eligendi neque rerum culpa minima? Aperiam, cupiditate.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae soluta esse facere est in dolore cupiditate sed voluptas doloribus inventore dicta, consequuntur necessitatibus, veritatis nobis ducimus reprehenderit ad quasi pariatur!
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, ullam asperiores, aliquid enim dolor saepe exercitationem unde eum corrupti nihil magnam incidunt? Fuga id consectetur quisquam nulla ipsam repellendus. Explicabo.
+        <img src="https://image.winudf.com/v2/image1/Y29tLmFqaW5mby53bG9zX3NjcmVlbl8wXzE2Mjk2OTEyMTNfMDg3/screen-0.jpg?fakeurl=1&type=.jpg" alt="" />
     </section>
 }

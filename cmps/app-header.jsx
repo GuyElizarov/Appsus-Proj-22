7744659866902a90ba05db3eafa4c9f6128fc606
Guy <guyelizarov@gmail.com>
2022-05-01 15:52:51 +0300

    v

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -14,17 +14,17 @@ class _AppHeader extends React.Component {
         return <header className="app-header layout">
             <NavLink to="/">
                 <div className='app-logo-wrapper'>
-                    <img className='app-logo' src='../assets/imgs/appsus-logo.png' />
+                    <img className='app-logo' src='assets/imgs/appsus-logo.png' />
                 </div>
             </NavLink>
             <nav>
-                <img className='menu-toggler' onClick={this.onToggleNavMenu} src='./../assets/imgs/menu-button.png' />
+                <img className='menu-toggler' onClick={this.onToggleNavMenu} src='assets/imgs/menu-button.png' />
 
                 <div className={this.state.isNavOpen ? 'nav-container open' : 'nav-container'}>
-                    <NavLink onClick={this.onToggleNavMenu} to="/note"><img src='./../assets/imgs/google-keep.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/book"><img src='./../assets/imgs/google-books.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/mail"><img src='./../assets/imgs/google-mail.png' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/" exact><img src='./../assets/imgs/google-home.png' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/note"><img src='assets/imgs/google-keep.png' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/book"><img src='assets/imgs/google-books.png' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/mail"><img src='assets/imgs/google-mail.png' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/" exact><img src='assets/imgs/google-home.png' /></NavLink>
                 </div>
                 {this.state.isNavOpen && <div className='nav-shadow' onClick={this.onToggleNavMenu}></div>}
             </nav>

1937e73a549860ee262e1d80953924b81e8e7066
Guy <guyelizarov@gmail.com>
2022-05-01 11:26:28 +0300

    v

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -9,20 +9,22 @@ class _AppHeader extends React.Component {
         this.setState({ isNavOpen: !currNavState })
     }
 
-        
+
     render() {
         return <header className="app-header layout">
-            <NavLink to="/"><div className='app-logo-wrapper'>
-                <img className='app-logo' src='../assets/imgs/appsus-logo.PNG' />
-            </div></NavLink>
+            <NavLink to="/">
+                <div className='app-logo-wrapper'>
+                    <img className='app-logo' src='./assets/imgs/appsus-logo.PNG' />
+                </div>
+            </NavLink>
             <nav>
-                <img className='menu-toggler' onClick={this.onToggleNavMenu} src='../assets/imgs/menu-button.PNG' />
+                <img className='menu-toggler' onClick={this.onToggleNavMenu} src='./assets/imgs/menu-button.PNG' />
 
                 <div className={this.state.isNavOpen ? 'nav-container open' : 'nav-container'}>
-                    <NavLink onClick={this.onToggleNavMenu} to="/note"><img src='../assets/imgs/google-keep.PNG' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/book"><img src='../assets/imgs/google-books.PNG' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/mail"><img src='../assets/imgs/google-mail.PNG' /></NavLink>
-                    <NavLink onClick={this.onToggleNavMenu} to="/" exact><img src='../assets/imgs/google-home.PNG' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/note"><img src='./assets/imgs/google-keep.PNG' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/book"><img src='./assets/imgs/google-books.PNG' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/mail"><img src='./assets/imgs/google-mail.PNG' /></NavLink>
+                    <NavLink onClick={this.onToggleNavMenu} to="/" exact><img src='./assets/imgs/google-home.PNG' /></NavLink>
                 </div>
                 {this.state.isNavOpen && <div className='nav-shadow' onClick={this.onToggleNavMenu}></div>}
             </nav>
